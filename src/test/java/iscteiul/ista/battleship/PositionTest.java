f1c91d8474e4d8fa5a864b6ee57c89f3d6b986a2
LETI-122653 <sheao@iscte-iul.pt>
2025-11-13 15:24:07 +0000

    adicionei teste unitario para o método getRow da classe Position

---
+++ src/test/java/iscteiul/ista/battleship/PositionTest.java
@@ -0,0 +1,50 @@
+package iscteiul.ista.battleship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+
+    @Test
+    void getRow() {
+        Position p = new Position(3, 5);
+        assertEquals(3, p.getRow());
+    }
+
+    @Test
+    void getColumn() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void isAdjacentTo() {
+    }
+
+    @Test
+    void occupy() {
+    }
+
+    @Test
+    void shoot() {
+    }
+
+    @Test
+    void isOccupied() {
+    }
+
+    @Test
+    void isHit() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
