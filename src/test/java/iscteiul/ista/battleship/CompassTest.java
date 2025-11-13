6367af7002ff8c533af25e09b46dbac771b6c272
LETI-122653 <sheao@iscte-iul.pt>
2025-11-13 14:47:18 +0000

    adicionei um teste unit para o getDirection na enum compass

---
+++ src/test/java/iscteiul/ista/battleship/CompassTest.java
@@ -0,0 +1,22 @@
+package iscteiul.ista.battleship;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Testes à enumeração Compass")
+class CompassTest {
+    @Test
+    @DisplayName("getDirection deve devolver o char associado a cada constante")
+    void testGetDirection() {
+        assertAll(
+                () -> assertEquals('n', Compass.NORTH.getDirection()),
+                () -> assertEquals('s', Compass.SOUTH.getDirection()),
+                () -> assertEquals('e', Compass.EAST.getDirection()),
+                () -> assertEquals('o', Compass.WEST.getDirection()),
+                () -> assertEquals('u', Compass.UNKNOWN.getDirection())
+        );
+    }
+
+}
