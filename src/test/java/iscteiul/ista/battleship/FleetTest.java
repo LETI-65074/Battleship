8b048a87561a933db2be57247bb6935f49f85349
LETI-122653 <sheao@iscte-iul.pt>
2025-11-14 16:31:05 +0000

    Adicionei teste unit para o método printShips da classe Fleet

---
+++ src/test/java/iscteiul/ista/battleship/FleetTest.java
@@ -0,0 +1,75 @@
+package iscteiul.ista.battleship;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FleetTest {
+
+    @Test
+    void printShips() {
+        // preparar lista de navios
+        List<IShip> ships = new ArrayList<>();
+        ships.add(new Barge(Compass.NORTH, new Position(0, 0)));
+        ships.add(new Barge(Compass.EAST, new Position(1, 1)));
+
+        // capturar System.out
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        try {
+            Fleet.printShips(ships);
+        } finally {
+            System.setOut(originalOut); // repor System.out
+        }
+
+        String output = outContent.toString();
+
+        assertAll(
+                () -> assertTrue(output.contains(ships.get(0).toString())),
+                () -> assertTrue(output.contains(ships.get(1).toString()))
+        );
+    }
+
+    @Test
+    void getShips() {
+    }
+
+    @Test
+    void addShip() {
+    }
+
+    @Test
+    void getShipsLike() {
+    }
+
+    @Test
+    void getFloatingShips() {
+    }
+
+    @Test
+    void shipAt() {
+    }
+
+    @Test
+    void printStatus() {
+    }
+
+    @Test
+    void printShipsByCategory() {
+    }
+
+    @Test
+    void printFloatingShips() {
+    }
+
+    @Test
+    void printAllShips() {
+    }
+}
